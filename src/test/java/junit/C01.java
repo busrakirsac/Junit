6eadc06411b5314bb847b48946aaea42677b6c7c
buusra <busrakirsac@gmail.com>
2024-01-08 21:01:12 +0300

    v1

--- src/test/java/junit/C01.java
+++ src/test/java/junit/C01.java
@@ -1,6 +1,41 @@
 package junit;
 
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.time.Duration;
+
 public class C01 {
 
+    @Test
+    public void test3() {
+        WebDriver driver = new FirefoxDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        driver.get("https://amazon.com");
+        driver.close();
+    }
+
+    @Test
+    public void test1() {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        driver.get("https://amazon.com");
+        driver.close();
+    }
+
+    @Test
+    public void test2() {
+        WebDriver driver = new EdgeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        driver.get("https://amazon.com");
+        driver.close();
+
+    }
 
 }
