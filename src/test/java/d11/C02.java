bf98e6347b861d49cb01caab4c6f1183950bcc72
buusra <busrakirsac@gmail.com>
2024-01-10 22:14:53 +0300

    v4

--- src/test/java/d11/C02.java
+++ src/test/java/d11/C02.java
@@ -1,4 +1,56 @@
 package d11;
 
-public class C02 {
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import utilities.TestBase;
+
+public class C02 extends TestBase {
+
+    @Test
+    public void test01() {
+        //https://jqueryui.com/droppable/ adresine gidelim
+        driver.get("https://jqueryui.com/droppable/");
+
+        //Drag me to my target webelementini Drop here webelementi üzerine bıkalım
+        Actions actions = new Actions(driver);
+        frameIndex(0);
+        WebElement drag = driver.findElement(By.xpath("//*[@id='draggable']"));
+        WebElement drop = driver.findElement(By.xpath("//*[@id='droppable']"));
+        actions.dragAndDrop(drag,drop).perform();
+    }
+
+    @Test
+    public void test02() {
+        //https://jqueryui.com/droppable/ adresine gidelim
+        driver.get("https://jqueryui.com/droppable/");
+
+        //Drag me to my target webelementini Drop here webelementi üzerine bıkalım
+        Actions actions = new Actions(driver);
+        frameIndex(0);
+        WebElement drag = driver.findElement(By.xpath("//*[@id='draggable']"));
+        WebElement drop = driver.findElement(By.xpath("//*[@id='droppable']"));
+        actions.clickAndHold(drag).
+                moveToElement(drop).
+                release().
+                perform();
+
+    }
+
+    @Test
+    public void test03() {
+        //https://jqueryui.com/droppable/ adresine gidelim
+        driver.get("https://jqueryui.com/droppable/");
+
+        //Drag me to my target webelementini Drop here webelementi üzerine bıkalım
+        Actions actions = new Actions(driver);
+        frameIndex(0);
+        WebElement drag = driver.findElement(By.xpath("//*[@id='draggable']"));
+        WebElement drop = driver.findElement(By.xpath("//*[@id='droppable']"));
+        actions.clickAndHold(drag).moveByOffset(186,48).release().perform();
+
+
+    }
+
 }
