0767372dedd63fe688b1d3f6d4bfd17cb882c86c
buusra <busrakirsac@gmail.com>
2024-01-12 20:58:16 +0300

    v4

--- src/test/java/d13/C03.java
+++ src/test/java/d13/C03.java
@@ -1,9 +1,15 @@
 package d13;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import utilities.TestBase;
+
+import java.time.Duration;
 
 public class C03 extends TestBase {
 
@@ -17,4 +23,41 @@ public class C03 extends TestBase {
         driver.findElement(By.id("twotabsearchtextbox")).sendKeys("No such element ", Keys.ENTER);
 
     }
+
+    @Test
+    public void test02() {
+
+        //https://the-internet.herokuapp.com/dynamic_loading/1
+        driver.get("https://the-internet.herokuapp.com/dynamic_loading/1");
+
+        //Start buttonuna tıkla
+        driver.findElement(By.xpath("//button")).click();
+
+
+        //Hello World! Yazının sitede oldugunu test et
+        WebElement helloWorld = driver.findElement(By.xpath("//h4[.='Hello World!']"));
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(2));
+        wait.until(ExpectedConditions.visibilityOf(helloWorld));
+        Assert.assertEquals("Hello World!", helloWorld.getText());
+    }
+
+
+    @Test
+    public void test03() {
+
+        //https://the-internet.herokuapp.com/dynamic_loading/1
+        driver.get("https://the-internet.herokuapp.com/dynamic_loading/1");
+
+        //Start buttonuna tıkla
+        driver.findElement(By.xpath("//button")).click();
+
+
+        //Hello World! Yazının sitede oldugunu test et
+
+        WebElement helloWorld = new WebDriverWait(driver, Duration.ofSeconds(20)).
+                until(ExpectedConditions.visibilityOfElementLocated(By.id("YANLIS LOCATE")));
+        Assert.assertEquals("Hello World!", helloWorld.getText());
+    }
+
+
 }
---
+++ src/test/java/d14/C01.java
@@ -0,0 +1,60 @@
+package d14;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import utilities.TestBase;
+
+import java.util.List;
+
+public class C01 extends TestBase {
+
+    @Test
+    public void test01() {
+
+        //amazon websayfasina gidelim
+        driver.get("https://amazon.com");
+
+        //arama kutusunda laptop aratalim
+        WebElement searchBox = driver.findElement(By.id("twotabsearchtextbox"));
+        searchBox.sendKeys("laptop");
+        searchBox.submit();
+
+        bekle(2);
+
+        //ana sayfaya dönelim
+        driver.navigate().back();
+        bekle(2);
+
+        //arama kutusunda desktop aratalim
+        WebElement searchBox2 = driver.findElement(By.id("twotabsearchtextbox"));
+        searchBox2.sendKeys("desktop");
+        searchBox2.submit();
+
+    }
+
+
+
+    @Test
+    public void test05() throws InterruptedException {
+        //- ebay sayfasına gidiniz
+        driver.get("https://ebay.com");
+
+        //electronics bölümüne tıklayınız
+        driver.findElement(By.xpath("(//*[text()='Electronics'])[2]")).click();
+
+        //Genişliği 225 ve Uzunluğu 225 olan resimlerin hepsine tıklayalım
+        //Her sayfanın sayfa başlığını yazdıralım
+        List<WebElement> products = driver.findElements(By.xpath("//*[@width='225' and @height='225']"));
+
+        for (int i = 0; i < products.size(); i++) {
+
+            // products = driver.findElements(By.xpath("//*[@width='225' and @height='225']"));
+            products.get(i).click();
+            Thread.sleep(2000);
+            System.out.println(i + 1 + ". title: " + driver.getTitle());
+            Thread.sleep(2000);
+            driver.navigate().back();
+            Thread.sleep(2000);
+        }
+}
